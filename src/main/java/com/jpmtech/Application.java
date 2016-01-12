ba6f57ea0a3e87f53476d22d26347aafa77ff679
Robert  Dinu <vjroby@gmail.com>
2016-01-12 07:49:37 +0200

    started csv parser

--- src/main/java/com/jpmtech/Application.java
+++ src/main/java/com/jpmtech/Application.java
@@ -5,6 +5,8 @@ import org.javamoney.moneta.Money;
 
 public class Application {
 
+    String csvFileName = "/jpm.csv";
+
     public static void main(String[] args){
         FastMoney fastMoney = FastMoney.of("GBP",23);
         Money money = Money.of("GBP", 1);
